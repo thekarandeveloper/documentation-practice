5d524346031ca0c27a236ccac6b35d43a4a00f20
Karan Kumar <contact.karankumar23@gmail.com>
2026-01-29 23:17:35 +0530

    Initial Commit

---
+++ documentation-practice/documentation_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  documentation_practiceApp.swift
+//  documentation-practice
+//
+//  Created by Karan Kumar on 29/01/26.
+//
+
+import SwiftUI
+
+@main
+struct documentation_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
